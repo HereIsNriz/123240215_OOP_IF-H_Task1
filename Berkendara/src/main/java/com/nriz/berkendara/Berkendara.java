10b0eef874036d9878218e0e427a7155e2ff27e6
Nriz <here.is.nriz@gmail.com>
2026-03-07 19:03:17 +0700

    Creating Packages and Classes First
    Create all needed packages and classes, there are 4 packages in total and each has their own classes.

---
+++ Berkendara/src/main/java/com/nriz/berkendara/Berkendara.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nriz.berkendara;
+
+/**
+ *
+ * @author acer
+ */
+public class Berkendara {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
